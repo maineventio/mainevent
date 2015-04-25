a56c339d8e7939b36a1b93d1399c1f9f783b777c
jmagnuss <jmagnuss@gmail.com>
2015-04-25 08:22:16 -0700

    JM

--- event-api/config/cache.php
+++ event-api/config/cache.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('CACHE_DRIVER', 'prod'),
+    'default' => env('CACHE_DRIVER', 'dev'),
 
     /*
     |--------------------------------------------------------------------------
@@ -36,7 +36,7 @@ return [
             'driver'  => 'memcached',
             'servers' => [
                 [
-                    'host' => env('MEMCACHED_HOST', 'eventcache.anallw.cfg.usw1.cache.amazonaws.com'), 'port' => env('MEMCACHED_PORT', 11211), 'weight' => 100
+                    'host' => env('MEMCACHED_HOST', '127.0.0.1'), 'port' => env('MEMCACHED_PORT', 11211), 'weight' => 100
                 ],
             ],
         ],
