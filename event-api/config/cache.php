55642a2e80510c47bb7b97cb0530c8504719c958
jmagnuss <jmagnuss@gmail.com>
2015-04-25 22:09:15 -0700

    JM:fixing endpoint

--- event-api/config/cache.php
+++ event-api/config/cache.php
@@ -36,7 +36,7 @@ return [
             'driver'  => 'memcached',
             'servers' => [
                 [
-                    'host' => env('MEMCACHED_HOST', '127.0.0.1'), 'port' => env('MEMCACHED_PORT', 11211), 'weight' => 100
+                    'host' => env('MEMCACHED_HOST', 'eventcache.anallw.cfg.usw1.cache.amazonaws.com'), 'port' => env('MEMCACHED_PORT', 11211), 'weight' => 100
                 ],
             ],
         ],
