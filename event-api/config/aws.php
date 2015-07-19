d5d829caa595d51dc3600f4da5339e56ebd6051f
Jeff Magnusson <jmagnuss@gmail.com>
2015-07-19 18:06:13 +0000

    JM: making AWS service provider and credentials work

--- event-api/config/aws.php
+++ event-api/config/aws.php
@@ -1,13 +1,25 @@
-return [
-'credentials' => [
-'key'    => 'YOUR_AWS_ACCESS_KEY_ID',
-'secret' => 'YOUR_AWS_SECRET_ACCESS_KEY',
-],
-'region' => 'us-west-2',
-'version' => 'latest',
+<?php
 
-// You can override settings for specific services
-'Ses' => [
-'region' => 'us-east-1',
-],
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | AWS SDK Configuration
+    |--------------------------------------------------------------------------
+    |
+    | The configuration options set in this file will be passed directly to the
+    | `Aws\Sdk` object, from which all client objects are created. The minimum
+    | required options are declared here, but the full set of possible options
+    | are documented at:
+    | http://docs.aws.amazon.com/aws-sdk-php/v3/guide/guide/configuration.html
+    |
+    */
+    'credentials' => [
+        'key'    => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+    ],
+    'region' => env('AWS_REGION', 'us-east-1'),
+    'version' => 'latest',
+
 ];
+
